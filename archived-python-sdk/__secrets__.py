0eb5042c61df203ed76abaed5e7204dcb6c25655
Pablion <36828324+Pablion@users.noreply.github.com>
2022-02-03 20:21:25 +0100

    refactor: archive old files

---
+++ archived-python-sdk/__secrets__.py
@@ -0,0 +1,9 @@
+# This file should be considered leaked. Please remove this line after next rotation.
+# This file should be kept secret, added in gitignore, and copied from artificial-dev-config repo.
+MNEMONIC1 = "monster sniff airport silent try this wheat style walnut anchor pond carry air letter sign matrix permit hope sentence canyon faculty strategy spider able indoor"
+MNEMONIC2 = "budget enemy ladder screen meat profit want appear humble village sick blur first wage junk fashion effort around sausage ostrich code airport fix ability shield"
+MNEMONIC3 = "symptom craft quote wisdom jungle debate split happy pause decline jump diet access entire calm cereal come clay crop winter volume release false abandon solve"
+ACCOUNT1_PUBLIC = "Z2CNMQ6JQQUBEOOJRDHTWVHJX55ZL7CRILNNBICBFY6Q5V7OFDG2IBYVFM"
+ACCOUNT2_PUBLIC = "CANWUHR4F2JXRSRMCDIAZI6S45HFO25WP4CMCPC2N73IE7K4C6AHFGWE5I"
+ACCOUNT3_PUBLIC = "S3OJFP27GULY6LVBAQVHPJIPJOZWZHETHIZO5HOU263UHZ7ZF4F6HWDAHU"
+PURE_STAKE_API_KEY = "WLJDqY55G5560kyCJVp647ERNZ5kJkdZ8OUdGNnV"
