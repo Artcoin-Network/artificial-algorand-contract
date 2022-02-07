b4d5959c8dfdd9f827c2a508d78591b12adf6c95
Pablion <36828324+Pablion@users.noreply.github.com>
2022-02-07 11:22:52 +0100

    feat: load secret

---
+++ artificial_algorand_contract/helper/__secrets__.py
@@ -0,0 +1,13 @@
+# This file should be considered leaked. Please remove this line after next rotation.
+# This file should be kept secret, added in gitignore, and copied from artificial-dev-config repo.
+from dotenv import dotenv_values
+
+secret = dotenv_values("./../../secret.env")
+
+ACCOUNT1_MNEMONIC = secret.get("REACT_APP_TEST_ACCOUNT1_MNEMONIC")
+ACCOUNT2_MNEMONIC = secret.get("REACT_APP_TEST_ACCOUNT2_MNEMONIC")
+ACCOUNT3_MNEMONIC = secret.get("REACT_APP_TEST_ACCOUNT3_MNEMONIC")
+ACCOUNT1_ADDRESS = secret.get("REACT_APP_TEST_ACCOUNT1_ADDRESS")
+ACCOUNT2_ADDRESS = secret.get("REACT_APP_TEST_ACCOUNT2_ADDRESS")
+ACCOUNT3_ADDRESS = secret.get("REACT_APP_TEST_ACCOUNT3_ADDRESS")
+PURE_STAKE_API_KEY = secret.get("REACT_APP_PURE_STAKE_API_KEY")
