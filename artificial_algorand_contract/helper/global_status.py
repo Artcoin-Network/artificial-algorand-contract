7c0805db2a03448a95f891849e910d4e5a546515
Pablion <36828324+Pablion@users.noreply.github.com>
2022-02-07 13:24:17 +0100

    feat: add algo client to status

--- artificial_algorand_contract/helper/global_status.py
+++ artificial_algorand_contract/helper/global_status.py
@@ -1,7 +1,21 @@
+from tkinter import N
+from typing import Literal
 from artificial_algorand_contract.helper.classes.algorand import AlgoAcc
-from . import __secrets__
+from .__secrets__ import (
+    ACCOUNT1_MNEMONICS,
+    ACCOUNT1_ADDRESS,
+    ACCOUNT2_MNEMONICS,
+    ACCOUNT2_ADDRESS,
+    ACCOUNT3_MNEMONICS,
+    ACCOUNT3_ADDRESS,
+    PURE_STAKE_API_KEY,
+)
+from algosdk.v2client import algod
 
+""" TYPES """
+Client_Type = Literal["pure_stake", "sandbox"]
 
+""" GLOBAL VARIABLES """
 status_initialized = False
 
 
@@ -12,27 +26,60 @@ class TestAccounts:
 
     def __init__(self) -> None:
         self.main_account = AlgoAcc(
-            mnemonics=__secrets__.ACCOUNT1_MNEMONIC,
-            address=__secrets__.ACCOUNT1_ADDRESS,
+            mnemonics=ACCOUNT1_MNEMONICS,
+            address=ACCOUNT1_ADDRESS,
         )
         self.alice = AlgoAcc(
-            mnemonics=__secrets__.ACCOUNT2_MNEMONIC,
-            address=__secrets__.ACCOUNT2_ADDRESS,
+            mnemonics=ACCOUNT2_MNEMONICS,
+            address=ACCOUNT2_ADDRESS,
         )
         self.bob = AlgoAcc(
-            mnemonics=__secrets__.ACCOUNT3_MNEMONIC,
-            address=__secrets__.ACCOUNT3_ADDRESS,
+            mnemonics=ACCOUNT3_MNEMONICS,
+            address=ACCOUNT3_ADDRESS,
         )
+
+
+class ClientInfo:
+    client_type: Client_Type
+    algod_address: str
+    algod_token: str
+    headers: dict | None
+
+    def __init__(self, client_type: Client_Type):
+        self.client_type = client_type
+        if client_type == "pure_stake":
+            self.algod_address = "https://testnet-algorand.api.purestake.io/ps2"
+            assert PURE_STAKE_API_KEY
+            self.algod_token = PURE_STAKE_API_KEY
+            self.headers = {
+                "X-API-Key": self.algod_token,
+            }
+        elif client_type == "sandbox":
+            print(f"Get client_type={client_type}. Using sandbox:")
+            self.algod_address = "http://localhost:4001"
+            self.algod_token = (
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            )
+        else:
+            raise ValueError(f"client_type={client_type} not supported.")
 
 
 class Status:
     accounts: TestAccounts
 
-    def __init__(self) -> None:
+    def __init__(self, client_type: Client_Type) -> None:
         global status_initialized
         if status_initialized:
             raise Exception("Status has already been initialized")
 
         self.accounts = TestAccounts()
-
+        self.client_info = ClientInfo(client_type)
+        self.client = algod.AlgodClient(
+            algod_token=self.client_info.algod_token,
+            algod_address=self.client_info.algod_address,
+            headers=self.client_info.headers,
+        )
         status_initialized = True
+
+    def initClient(self) -> None:
+        pass
