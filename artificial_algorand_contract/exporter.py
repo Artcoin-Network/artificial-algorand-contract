2c408ffcc5b2fbf0fa8307a5465ef6aa11cd5087
Pablion <36828324+Pablion@users.noreply.github.com>
2022-02-07 09:35:18 +0100

    add an teal exporter

--- artificial_algorand_contract/exporter.py
+++ artificial_algorand_contract/exporter.py
@@ -1,9 +1,12 @@
 ## non-package imports
+from json import dump as json_dump
 from pathlib import Path
-from counter.counter_contract import approval_program, clear_program
+from typing import Callable
+from counter.counter_contract import *  # or next line
 
+# from counter.counter_contract import approval_program, clear_program, state_storage
 ## package imports
-# from counter.counter_contract import approval_program, clear_program
+# from counter.counter_contract import approval_program, clear_program, state_storage
 
 # TODO: maybe add a "topic" and add this string to filename.
 
@@ -11,16 +14,24 @@ REPO_ROOT = Path(__file__).resolve().parent.parent
 OUTPUT_DIR = REPO_ROOT / "output"
 
 
-def write_output(filename, compile_func):
+def write_output_teal(filename: str, compile_func: Callable) -> None:
     output_path = OUTPUT_DIR / filename
     with open(output_path, "w") as f:
         f.write(compile_func())
 
 
+def write_json_config(filename: str, config_func: Callable) -> None:
+    output_path = OUTPUT_DIR / filename
+    with open(output_path, "w") as f:
+        json_dump(config_func(), f, indent=4)
+
+
 def main():
-    write_output("approval.teal", approval_program)
-    write_output("clear.teal", clear_program)
+    write_output_teal("approval.teal", approval_program)
+    write_output_teal("clear.teal", clear_program)
+    write_json_config("state.json", storage_state)
 
 
 if __name__ == "__main__":
     main()
+    print("finished")
