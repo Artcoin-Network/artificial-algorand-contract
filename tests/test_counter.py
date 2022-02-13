65d9c153dbaab1ea3660a3d918e3db1401398bf9
Bo Yao <bo@near.org>
2022-02-13 15:16:57 +0800

    test single file

---
+++ tests/test_counter.py
@@ -0,0 +1,14 @@
+from artificial_algorand_contract.tests import counter_full_test
+
+counter_full_test()
+
+
+# from artificial_algorand_contract.classes.account import check_main
+# from artificial_algorand_contract.classes.asset import create_test_asset
+# create_test_asset()
+# check_main()
+print("finished")
+
+# MEMO: not deleted 69879104
+# TODO0209: enable tester.acc.call()
+# TODO0209: functions like call, opt_in etc should be in the tester.acc
