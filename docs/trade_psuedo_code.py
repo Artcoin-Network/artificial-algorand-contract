0eeb1063525542eec8e763dcad4fd683b434da2f
Pablion <36828324+Pablion@users.noreply.github.com>
2022-03-28 01:04:45 +0200

    docs: fix fee

--- docs/trade_psuedo_code.py
+++ docs/trade_psuedo_code.py
@@ -1,6 +1,11 @@
+from typing import Any
+
+
 class Exchange:  # the exchange, with prop `users`
     global_aAsset: list[int]
     users: list[User]  # type: ignore
+    aStocks: list[int]
+    ZKP: Any
 
     def __init__(self) -> None:
         pass
@@ -17,7 +22,25 @@ class User:
         self.pk = pk
         self.aUSD = 0
 
+    def _validate_trading_period(self, token_id: int) -> bool:
+        if token_id in self.ex.aStocks:
+            import datetime
+
+            return (
+                self.ex.ZKP.price_info(token_id).last_update_time
+                > datetime.datetime.now().timestamp()
+            )
+
+        return True
+
+    def _before_all(self, token_id) -> bool:
+        if self._validate_trading_period(token_id):
+            return True
+        return False
+
     def buy(self, token_id: int, token_price: float, aUSD: int) -> None:
+        if not self._before_all(token_id):
+            return None
         self.aUSD -= aUSD
         fee = aUSD * 0.001
         aUSD = int(aUSD - fee)
@@ -26,6 +49,8 @@ class User:
         self.ex.global_aAsset[token_id] += token_amount
 
     def sell(self, token_id: int, token_price: float, aUSD: int) -> None:
+        if not self._before_all(token_id):
+            return None
         self.aUSD += aUSD
         fee = aUSD * 0.001
         aUSD = int(aUSD - fee)
